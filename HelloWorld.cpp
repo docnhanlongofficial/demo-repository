53fdf05986118137f031d302990c284d407be319
Phan Nguyen Xuan Phuc <docnhanlongofficial.com>
2021-10-09 22:58:15 +0700

    New Update 22:55
    Modified HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout<<"Welcome Xuan Phuc to Git & GitHub (^-^)"<<std::endl;
+    std::cout<<"Welcome Xuan Phuc to Git and GitHub (^-^)"<<std::endl;
     return 0;
 }
--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Name: Phan Nguyen Xuan Phuc
 ID: ITITIU19042
 Major: Computer Engineering
 Hobbies: Games, Programming, Walking
+
+## Update History
+NEW Update: 22:40 PM
+NEW Update: 22:55 PM
